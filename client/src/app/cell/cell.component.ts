4420500bc7df353f2419f2c554898b1a0d514686
lcross2002 <liamdcross@outlook.com>
2024-02-03 17:57:33 +0000

    overhaul maze display + how to play

--- client/src/app/cell/cell.component.ts
+++ client/src/app/cell/cell.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CellType, MazeService } from '../../services/maze.service';
 import { QuizService } from '../../services/quiz.service';
 
@@ -10,19 +10,28 @@ import { QuizService } from '../../services/quiz.service';
   templateUrl: './cell.component.html',
   styleUrl: './cell.component.scss'
 })
-export class CellComponent {
+export class CellComponent implements OnInit {
   @Input() i!: number;
   @Input() j!: number;
   @Input() cell!: CellType;
+
+  cellType = CellType;
+  clicked: boolean = false;
 
   constructor(
     public maze: MazeService,
     public quiz: QuizService,
   ) {}
 
+  ngOnInit() {
+    if (this.cell === CellType.entrance)
+      this.clicked = true;
+  }
+
   cellClick() {
     if (this.cell === CellType.empty) {
       this.maze.updateFog(this.i, this.j);
+      this.clicked = true;
     } else if (this.cell === CellType.puzzle) {
       this.quiz.openQuiz();
     }
