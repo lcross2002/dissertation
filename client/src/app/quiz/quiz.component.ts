a31094ae335d2550fe5ad14f5f28e709e4a4b5de
lcross2002 <liamdcross@outlook.com>
2024-02-05 13:18:22 +0000

    send quiz to component + various stylings

--- client/src/app/quiz/quiz.component.ts
+++ client/src/app/quiz/quiz.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener, OnDestroy } from '@angular/core';
-import { QuizService } from '../../services/quiz.service';
+import { QuizService, IMultiQuiz, IFillQuiz } from '../../services/quiz.service';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -12,18 +12,32 @@ import { Subscription } from 'rxjs';
 })
 export class QuizComponent implements OnDestroy {
   
+  multiQuiz?: IMultiQuiz;
+  fillQuiz?: IFillQuiz;
   open: boolean = false;
 
   private subscription: Subscription;
 
   constructor(public quiz: QuizService) {
-    this.subscription = this.quiz.open$.subscribe(() => {
+    this.subscription = this.quiz.open$.subscribe((q) => {
+      if (q.quizType === 'multi') {
+        this.multiQuiz = q;
+        this.fillQuiz = undefined;
+      } else if (q.quizType === 'fill') {
+        this.fillQuiz = q;
+        this.multiQuiz = undefined;
+      }
+
       this.open = true;
     });
   }
 
   close() {
     this.open = false;
+  }
+
+  multiClick(id: string) {
+    console.log(id);
   }
 
   @HostListener('document:keydown.escape', ['$event'])
--- client/src/services/quiz.service.ts
+++ client/src/services/quiz.service.ts
@@ -1,17 +1,47 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 
-interface quiz {
 
+export interface IMultiAnswer {
+  id: string;
+  value: string;
 }
+
+export interface IMultiQuiz {
+  quizType: 'multi';
+  question: string;
+  answers: IMultiAnswer[];
+  correctId: string;
+}
+
+export interface IFillQuiz {
+  quizType: 'fill';
+  before: string;
+  after: string;
+  answer: string;
+}
+
+export type Quiz = IMultiQuiz | IFillQuiz;
 
 @Injectable({providedIn: 'root'})
 export class QuizService {
 
-  open$: Observable<quiz>;
-  private openSubject: Subject<quiz>;
+  open$: Observable<Quiz>;
+  private openSubject: Subject<Quiz>;
   private quizIndex: number = 0;
-  private quizzes: quiz[] = [];
+  private quizzes: Quiz[] = [
+    {
+      quizType: 'multi',
+      question: 'test',
+      answers: [
+        {id: 'A', value: 'test'},
+        {id: 'B', value: 'test'},
+        {id: 'C', value: 'test'},
+        {id: 'D', value: 'test'}
+      ],
+      correctId: 'A'
+    }
+  ];
   
   constructor() {
     this.openSubject = new Subject();
@@ -22,7 +52,7 @@ export class QuizService {
     this.openSubject.next(this.generateQuiz());
   }
 
-  private generateQuiz(): quiz {
+  private generateQuiz(): Quiz {
     return this.quizzes[this.quizIndex];
   }
 }
